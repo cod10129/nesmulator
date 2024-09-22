1c4e749eb08a423901df15059ee2cf46ecbd4073
cod10129 <110200933+cod10129@users.noreply.github.com>
2024-09-22 17:30:27 -0500

    fix test for real

--- crates/nesfile/tests/roms_do_parse.rs
+++ crates/nesfile/tests/roms_do_parse.rs
@@ -6,8 +6,6 @@ fn super_mario_bros_parses() {
     let manifest_path = Path::new(env!("CARGO_MANIFEST_DIR"));
     let path = manifest_path.join("tests/Super Mario Bros.nes");
     let data = std::fs::read(&path).unwrap();
-    let (i, file) = File::parse(&data).unwrap();
+    let (i, _file) = File::parse(&data).unwrap();
     assert!(i.is_empty());
-    dbg!(file);
-    panic!()
 }
